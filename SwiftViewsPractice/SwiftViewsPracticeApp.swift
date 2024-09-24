a557f77150d9115e376bf2650d91b62589423e62
Scott Crowder <scrowder@gmail.com>
2024-09-24 11:12:03 -0500

    Initial Commit

---
+++ SwiftViewsPractice/SwiftViewsPracticeApp.swift
@@ -0,0 +1,20 @@
+//
+// File: SwiftViewsPracticeApp.swift
+// Project: SwiftViewsPractice
+// 
+// Created by SCOTT CROWDER on 9/24/24.
+// 
+// Copyright © Playful Logic Studios, LLC 2024. All rights reserved.
+// 
+
+
+import SwiftUI
+
+@main
+struct SwiftViewsPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
